1c59e3e83c4d4997416fd78139b93fdcd8d65a5b
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-05 18:35:51 +0530

    hints/api

--- routes/hints.js
+++ routes/hints.js
@@ -5,13 +5,16 @@ app.get("/", async (req, res) => {
   let currentPass = await passwordSchema.findOne({
     year: new Date().getFullYear(),
   });
+
   if (!currentPass) return res.send("No password found");
+
   let publishedHints = currentPass.hints.filter((hint) => {
     if (!hint.published) {
       hint.description = "This hint has not been published yet";
     }
     return hint;
   });
+
   res.send({
     hints: publishedHints,
     totalHints: currentPass.hints.length,
