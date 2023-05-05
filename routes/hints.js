cf8d3f814faaac604338279572dd112b3e9a6bc6
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-05 17:35:45 +0530

    routes

--- routes/hints.js
+++ routes/hints.js
@@ -2,14 +2,15 @@ const app = require("express").Router();
 const passwordSchema = require("../db/passwordSchema");
 
 app.get("/", async (req, res) => {
-  const allPasswords = await passwordSchema.find({
+  let currentPass = await passwordSchema.findOne({
     year: new Date().getFullYear(),
   });
-
-  const currentPass = allPasswords[0];
   if (!currentPass) return res.send("No password found");
   const publishedHints = currentPass.hints.filter((hint) => hint.published);
-  res.send(publishedHints);
+  res.send({
+    hints: publishedHints,
+    totalHints: currentPass.hints.length,
+  });
 });
 
 module.exports = app;
